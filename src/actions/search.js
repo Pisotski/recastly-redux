dcd3c1cdf6fa497eb78e506807dd529313c1c9e6
Poisk <vpisotski@gmail.com>
2018-07-10 17:47:29 -0700

    passed test upon store

--- src/actions/search.js
+++ src/actions/search.js
@@ -1,12 +1,20 @@
-import searchYouTube from '../lib/searchYouTube.js';
-import changeVideoList from './videoList.js';
-import changeVideo from './currentVideo.js';
+import axios from 'axios';
+import searchYouTube from '../lib/searchYouTube.js'; //why?
+import changeVideoList from './videoList.js'; //why?
+import changeVideo from './currentVideo.js'; //why?
 import YOUTUBE_API_KEY from '../config/youtube.js';
 
+const handleVideoSearch = (q) => {
 
-var handleVideoSearch = (q) => {
- 
-  //TODO:  Write an asynchronous action to handle a video search!
+  return (dispatch) => (
+    searchYouTube({
+      key: YOUTUBE_API_KEY,
+      query: q,
+    }, (videos) => {
+      changeVideo(videos[0]);
+      changeVideoList(videos);
+    })
+  );
 };
 
 export default handleVideoSearch;
